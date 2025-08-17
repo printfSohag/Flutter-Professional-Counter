3fb9888b8c4fc1c9cdfd7ab01badcd46b2c386a8
Sohag <printfsohag@gmail.com>
2025-08-17 21:43:34 +0600

    feat: Build counter screen UI

--- lib/features/counter/counter_screen.dart
+++ lib/features/counter/counter_screen.dart
@@ -11,7 +11,26 @@ class _CounterScreenState extends State<CounterScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(title: Text('Professional Counter')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('0', style: TextStyle(fontSize: 100, fontWeight: FontWeight.bold), ),
+            SizedBox(height: 10),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                ElevatedButton(onPressed: () {}, child: Text('Reset')),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: () {}, child: Text('Add')),
+                SizedBox(width: 10),
+                ElevatedButton(onPressed: () {}, child: Text('Subtract')),
+              ],
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
