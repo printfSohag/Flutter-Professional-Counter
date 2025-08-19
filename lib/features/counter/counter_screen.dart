2bbd1f87ee89c0ade049f177a76cb6c3f105dfc5
Sohag <printfsohag@gmail.com>
2025-08-19 13:46:01 +0600

    Added Sharedpreferences to save data

--- lib/features/counter/counter_screen.dart
+++ lib/features/counter/counter_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class CounterScreen extends StatefulWidget {
 
@@ -12,20 +13,44 @@ class CounterScreen extends StatefulWidget {
 class _CounterScreenState extends State<CounterScreen> {
 
   int _counter = 0;
+
+  @override
+  void initState() {
+    super.initState();
+    _loadCounterState();
+  }
+
+
+  Future<void> _loadCounterState() async{
+    final prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _counter = prefs.getInt('sohag') ?? 0;
+    });
+  }
+
+  Future<void> _saveCounterState() async{
+    final prefs = await SharedPreferences.getInstance();
+    await prefs.setInt('sohag', _counter);
+    print(_counter);
+  }
+
   void _increment(){
     setState((){
       _counter++;
     });
+    _saveCounterState();
   }
   void _decrement(){
     setState((){
       _counter > 0 ? _counter-- : null;
     });
+    _saveCounterState();
   }
   void _reset(){
     setState((){
       _counter = 0;
     });
+    _saveCounterState();
   }
   void _doubleButton(){
     setState((){
@@ -33,6 +58,7 @@ class _CounterScreenState extends State<CounterScreen> {
         _counter *= 2;
       }
     });
+    _saveCounterState();
   }
   @override
   Widget build(BuildContext context) {
